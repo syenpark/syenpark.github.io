a4ba7f5ee895314abfbd584ec9af9b054dac63df
Syen Park <syenpark@gmail.com>
2021-02-06 16:42:38 +0800

    Add trains for 3 cities

--- _posts/2021-01-29-get-on-the-train.md
+++ _posts/2021-01-29-get-on-the-train.md
@@ -6,33 +6,72 @@ date:   2021-01-29
 categories: data_science
 modified_date: 2021-02-06
 ---
-Data visualisation project for housing prices
+Data visualisation for housing prices in three cities.
 
-Hong Kong
+### __1. Hong Kong__
+The city in which I am currently living.  
 
 <html>
   <head>
     <style>
-      #train {
+      #trainHKG {
         position: relative;
         cursor: pointer;
       }
     </style>
   </head>
   <body>
-    <img id="train" src="https://js.cx/clipart/train.gif">
+    <img id="trainHKG" src="https://js.cx/clipart/train.gif">
     <br/><br/>
-    <button type="button" onclick="myFunction()">Try it</button>
+    <button type="button" onclick="trainHKG()">Try it</button>
+    <br/><br/>
     <script src="{{ site.baseurl }}{% link assets/js/2021-01-29-get-on-the-train.js %}"></script>
   </body>
 </html>
 
+### __2. Seoul__
+My hometown.  
+
+<html>
+  <head>
+    <style>
+      #trainSEL {
+        position: relative;
+        cursor: pointer;
+      }
+    </style>
+  </head>
+  <body>
+    <img id="trainSEL" src="https://js.cx/clipart/train.gif">
+    <br/><br/>
+    <button type="button" onclick="trainSEL()">Try it</button>
+    <br/><br/>
+    <script src="{{ site.baseurl }}{% link assets/js/2021-01-29-get-on-the-train.js %}"></script>
+  </body>
+</html>
+
+### __3. Tokyo__
+The unprecedented rise in house prices and market crash.  
 
 
-Seoul
-
-Tokyo
+<html>
+  <head>
+    <style>
+      #trainTYO {
+        position: relative;
+        cursor: pointer;
+      }
+    </style>
+  </head>
+  <body>
+    <img id="trainTYO" src="https://js.cx/clipart/train.gif">
+    <br/><br/>
+    <button type="button" onclick="trainTYO()">Try it</button>
+    <br/><br/>
+    <script src="{{ site.baseurl }}{% link assets/js/2021-01-29-get-on-the-train.js %}"></script>
+  </body>
+</html>
 
 To be updated...
 
-References: [JavaScript animations](https://javascript.info/js-animation) and [howto_js_animate](https://www.w3schools.com/howto/howto_js_animate.asp)
+References: [JavaScript animations](https://javascript.info/js-animation)
--- assets/js/2021-01-29-get-on-the-train.js
+++ assets/js/2021-01-29-get-on-the-train.js
@@ -1,12 +1,69 @@
-function myFunction() {
-  let start = Date.now();
+// Reference: https://javascript.info/js-animation
+function animate(options) {
 
-  let timer = setInterval(function() {
-  let timePassed = Date.now() - start;
+  var start = performance.now();
 
-  document.getElementById("train").style.left = timePassed / 5 + 'px';
+  requestAnimationFrame(function animate(time) {
+    // timeFraction от 0 до 1
+    var timeFraction = (time - start) / options.duration;
+    if (timeFraction > 1) timeFraction = 1;
 
-  if (timePassed > 2000) clearInterval(timer);
+    // текущее состояние анимации
+    var progress = options.timing(timeFraction)
 
-  }, 20);
+    options.draw(progress);
+
+    if (timeFraction < 1) {
+      requestAnimationFrame(animate);
+    }
+
+  });
+}
+
+function trainHKG() {
+  animate({
+    duration: 7000,
+    timing: function bounce(timeFraction) {
+      for (let a = 0, b = 1, result; 1; a += b, b /= 2) {
+        if (timeFraction >= (7 - 4 * a) / 11) {
+          return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
+        }
+      }
+    },
+    draw: function(progress) {
+      document.getElementById("trainHKG").style.left = progress * 570 + 'px';
+    }
+  });
+}
+
+function trainSEL() {
+  animate({
+    duration: 7000,
+    timing: function bounce(timeFraction) {
+      for (let a = 0, b = 1, result; 1; a += b, b /= 2) {
+        if (timeFraction >= (7 - 4 * a) / 11) {
+          return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
+        }
+      }
+    },
+    draw: function(progress) {
+      document.getElementById("trainSEL").style.left = progress * 570 + 'px';
+    }
+  });
+}
+
+function trainTYO() {
+  animate({
+    duration: 7000,
+    timing: function bounce(timeFraction) {
+      for (let a = 0, b = 1, result; 1; a += b, b /= 2) {
+        if (timeFraction >= (7 - 4 * a) / 11) {
+          return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
+        }
+      }
+    },
+    draw: function(progress) {
+      document.getElementById("trainTYO").style.left = progress * 570 + 'px';
+    }
+  });
 }
